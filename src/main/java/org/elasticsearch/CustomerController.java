49f7326cfe12ddd4d2694996c030676c575ce321
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-11-06 17:02:46 +0200

    create rest controller

---
+++ src/main/java/org/elasticsearch/CustomerController.java
@@ -0,0 +1,20 @@
+package org.elasticsearch;
+
+import org.elasticsearch.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+  /*  @Autowired
+    HtmlGeneratorService htmlGeneratorService;*/
+
+    @RequestMapping(value = "createCustomer", method = RequestMethod.POST)
+    public String createCustomer(@RequestBody Customer request) throws Exception {
+        return null;
+    }
+}
